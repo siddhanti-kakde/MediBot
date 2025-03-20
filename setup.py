8c920e2e1aa904c7c2b9e5a30e85f74345fac137
UditShahu <shahuudit@gmail.com>
2025-03-20 19:31:44 +0530

    Folder structure added

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Generative AI Project',
+    name = 'MedChat',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Udit Shahu',
+    author_email= 'shahuudit@gmail.com',
     packages= find_packages(),
     install_requires = []
 
